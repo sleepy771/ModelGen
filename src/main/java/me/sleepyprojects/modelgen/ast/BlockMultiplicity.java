8671dda372fa15a5791d544d40fedc8056c3e8f1
Filip Hornak <filip.hornak@comsource.cz>
2016-12-04 19:51:29 +0100

    add values to BlockMultiplicity

--- src/main/java/me/sleepyprojects/modelgen/ast/BlockMultiplicity.java
+++ src/main/java/me/sleepyprojects/modelgen/ast/BlockMultiplicity.java
@@ -8,5 +8,18 @@ package me.sleepyprojects.modelgen.ast;
  * @since 24.11.16
  */
 public enum BlockMultiplicity {
-    ONE, TWO, MULTIPLE, NONE
+    ONE(1),
+    TWO(2),
+    MULTIPLE(-1),
+    NONE(0);
+
+    private final int size;
+
+    BlockMultiplicity(int size) {
+        this.size = size;
+    }
+
+    public int size() {
+        return size;
+    }
 }
